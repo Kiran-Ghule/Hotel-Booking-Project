3e94df2b2f4ba090fcfd107dc481f13fed0d84e1
Kiran-Ghule <ghulekiran1608@gmail.com>
2026-09-24 22:56:46 +0530

    Handler for AccessDenie and JWT Exception...

--- src/main/java/com/airbnb/project/restAdvices/GlobalExceptionHandler.java
+++ src/main/java/com/airbnb/project/restAdvices/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.airbnb.project.restAdvices;
 
 import com.airbnb.project.exceptions.ResourceNotFound;
+import io.jsonwebtoken.JwtException;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +14,31 @@ import java.util.List;
 @RestControllerAdvice
 @Data
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiError> handleJwtException(JwtException e) {
+        ApiError error = ApiError.builder()
+                .status(HttpStatus.UNAUTHORIZED)
+                .message(e.getMessage())
+                .errors(null)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+    }
+
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiError> handleAccessDeniedException(AccessDeniedException e) {
+        ApiError error = ApiError.builder()
+                .status(HttpStatus.FORBIDDEN)
+                .message(e.getMessage())
+                .errors(null)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
+    }
+
+
 
     @ExceptionHandler(ResourceNotFound.class)
     public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFound exception) {
@@ -34,5 +61,7 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
+
+
 
 }
